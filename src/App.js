d76847a1fb8be3e697f77d3b6dd5071517c1033f
renee.troiani@gmail.com <renee.troiani@gmail.com>
2022-02-25 12:05:02 -0700

    set up route to home and pizza form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
+import { Route, Link, Switch } from 'react-router-dom';
+
 
 const App = () => {
   return (
     <>
+    <nav>
       <h1>Lambda Eats</h1>
+        <div className='nav-links'>
+          <Link to='/'>Home</Link>
+          <Link to='/pizza'>Order</Link>
+        </div>
+    </nav>
       <p>You can remove this code and create your own header</p>
+   
+    </Switch>
     </>
   );
 };
