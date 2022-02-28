1f9c939723bc2fed4ca9dafc8317b3b47ddc70a5
renee.troiani@gmail.com <renee.troiani@gmail.com>
2022-02-28 10:46:28 -0700

    toppings and special instructions appear after submission

--- src/Pizza.js
+++ src/Pizza.js
@@ -5,16 +5,20 @@ export default function Pizza(props) {
     
 
     if(!orders) {
-        return <h3>fetching your pizza orders!</h3>
+        return <h3>fetching your pizza order!</h3>
     }
 
     return (
         <div className='pizza-container'>
             <h2>{orders.name}'s Order</h2>
             <p>Size: {orders.size}</p>
+            <ul>Toppings:
+                {orders.topping1 ? <li>Pepperoni</li> : null}
+                {orders.topping2 ? <li>Ham</li> : null}
+                {orders.topping3 ? <li>Sausage</li> : null}
+                {orders.topping4 ? <li>Peppers</li> : null}
+            </ul>
             <p>Instructions: {orders.special}</p>
-            <p>Toppings:</p>
-            {/* {if(order.topping1 } */}
         </div>
     )
 }
